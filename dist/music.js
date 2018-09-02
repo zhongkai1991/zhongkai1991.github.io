cf043a7c2491be0da66bf4971855a743c2aa7d1d
zhongkai1991 <hizhongkai@126.com>
2018-09-01 23:40:57 -0400

    Site updated: 2018-09-01 23:40:56

--- dist/music.js
+++ dist/music.js
@@ -6,13 +6,13 @@ const ap = new APlayer({
       {
         name: "Going Home",
         artist: 'Kenny G',
-        url: 'http://music.163.com/song/media/outer/url?id=1615675.mp3',
+        url: 'http://fs.open.kugou.com/1d52ec9d21bf0d43b8bebee3e44cd4dd/5b8b5b2e/G007/M01/0F/0D/Rw0DAFS1hnaAPpe5AJFi9SWZmjQ224.mp3',
         cover: 'http://perryponders.com/wp-content/uploads/2015/09/kennyg.jpg',
       },
       {
         name: 'if cloud knows',
         artist: 'Valen Hsu',
-        url: 'http://music.163.com/song/media/outer/url?id=307514.mp3',
+        url: '//m128.xiami.net/330/2330/12587/383367_1512721472890.mp3?auth_key=1536462000-0-0-78d3563923b340e452209e42193f7b69',
         cover: 'https://i.ytimg.com/vi/FRbaawcE3MI/hqdefault.jpg',
       },
       {
