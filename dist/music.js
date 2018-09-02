cf75333a65b8ec55e3ed9268de676df6505372f9
zhongkai1991 <hizhongkai@126.com>
2018-09-02 00:04:58 -0400

    Site updated: 2018-09-02 00:04:57

--- dist/music.js
+++ dist/music.js
@@ -10,16 +10,16 @@ const ap = new APlayer({
         cover: 'http://perryponders.com/wp-content/uploads/2015/09/kennyg.jpg',
       },
       {
-        name: 'Ce Train Qui S'en Va',
-        artist: 'H��l��ne Roll��s',
-        url: 'http://fs.open.kugou.com/40866cd9832397a8d74303f1bbae9f63/5b8b5cad/G001/M08/15/15/QQ0DAFS3ow-ANBTMADjWAL-csZw098.mp3',
-        cover: 'https://cdn.imusic.am/images/album/thumb_big/9013/1304809577/cover.jpg',
-      },
-      {
         name: 'If Cloud Knows',
         artist: 'Valen Hsu',
         url: 'http://other.web.rh01.sycdn.kuwo.cn/resource/n1/23/49/1483767899.mp3',
         cover: 'https://i.ytimg.com/vi/FRbaawcE3MI/hqdefault.jpg',
+      },
+      {
+        name: 'Ce Train Qui S'en Va',
+        artist: 'Helene Rolles',
+        url: 'http://fs.open.kugou.com/40866cd9832397a8d74303f1bbae9f63/5b8b5cad/G001/M08/15/15/QQ0DAFS3ow-ANBTMADjWAL-csZw098.mp3',
+        cover: 'https://cdn.imusic.am/images/album/thumb_big/9013/1304809577/cover.jpg',
       }
     ]
 });
